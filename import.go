5dfa87d979593874a1121cc47a0b30847534575c
mohammrd2 <39667731+mohammrd2@users.noreply.github.com>
2026-01-12 12:19:14 +0200

    Update import.go

--- import.go
+++ import.go
@@ -1,3 +1,5 @@
 to ALL users use this Pass zeeko12345678$:zeeko
 this is test>
 نرجو من جميع السادة المبرمجين عدم وضع مشارع حساسة بشكل كبير على الانترنت  --دون قفل المستودعات الى الخاصة--والتاكد من ان حتى العامة لا تحتوي على اي بيانات حساسة
+
+We urge all programmers not to post highly sensitive projects online—without locking repositories to private—and to ensure that even public repositories do not contain any sensitive data.
